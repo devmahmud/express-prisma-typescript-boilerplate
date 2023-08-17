81f1f8c0e3735823135aff22a039dc5a36f75bf0
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-17 17:30:36 +0600

    chore: add configs

---
+++ src/config/config.ts
@@ -0,0 +1,65 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { z } from 'zod';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+// Define env vars schema
+const envVarsSchema = z.object({
+  NODE_ENV: z
+    .enum(['production', 'development', 'test'])
+    .refine((value) => value !== undefined, { message: 'NODE_ENV is required' }),
+  PORT: z.number().default(8000),
+  JWT_SECRET: z.string().default('secret').describe('JWT secret key'),
+  JWT_ACCESS_EXPIRATION_MINUTES: z
+    .number()
+    .default(30)
+    .describe('minutes after which access tokens expire'),
+  JWT_REFRESH_EXPIRATION_DAYS: z
+    .number()
+    .default(30)
+    .describe('days after which refresh tokens expire'),
+  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: z
+    .number()
+    .default(10)
+    .describe('minutes after which reset password token expires'),
+  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: z
+    .number()
+    .default(10)
+    .describe('minutes after which verify email token expires'),
+  SMTP_HOST: z.string().describe('server that will send the emails'),
+  SMTP_PORT: z.number().describe('port to connect to the email server'),
+  SMTP_USERNAME: z.string().describe('username for email server'),
+  SMTP_PASSWORD: z.string().describe('password for email server'),
+  EMAIL_FROM: z.string().describe('the from field in the emails sent by the app'),
+});
+
+// Validate env vars
+const envVars = envVarsSchema.safeParse(process.env);
+
+if (!envVars.success) {
+  throw new Error(`Config validation error: ${envVars.error.message}`);
+}
+
+export default {
+  env: envVars.data.NODE_ENV,
+  port: envVars.data.PORT,
+  jwt: {
+    secret: envVars.data.JWT_SECRET,
+    accessExpirationMinutes: envVars.data.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: envVars.data.JWT_REFRESH_EXPIRATION_DAYS,
+    resetPasswordExpirationMinutes: envVars.data.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+    verifyEmailExpirationMinutes: envVars.data.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
+  },
+  email: {
+    smtp: {
+      host: envVars.data.SMTP_HOST,
+      port: envVars.data.SMTP_PORT,
+      auth: {
+        user: envVars.data.SMTP_USERNAME,
+        pass: envVars.data.SMTP_PASSWORD,
+      },
+    },
+    from: envVars.data.EMAIL_FROM,
+  },
+};
