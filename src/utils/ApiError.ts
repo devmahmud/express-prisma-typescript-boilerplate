349192e136b418477240768c9012aaa988093c02
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-17 17:42:15 +0600

    chore: custom error format

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,18 @@
+// Custom error class with status code and isOperational property
+class ApiError extends Error {
+  statusCode: number;
+  isOperational: boolean;
+
+  constructor(statusCode: number, message: string | undefined, isOperational = true, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
