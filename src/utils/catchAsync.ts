fe2bf260cc950736eb2413f30f8f6bb19c71c253
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-17 17:43:44 +0600

    chore: catchAsync common error handler

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+import { Request, Response, NextFunction } from 'express-serve-static-core';
+
+export interface CustomParamsDictionary {
+  [key: string]: any;
+}
+
+// Common async function wrapper to catch errors
+const catchAsync =
+  (fn: RequestHandler<CustomParamsDictionary, any, any, qs.ParsedQs, Record<string, any>>) =>
+  (
+    req: Request<CustomParamsDictionary, any, any, any, Record<string, any>>,
+    res: Response<any, Record<string, any>, number>,
+    next: NextFunction
+  ) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
+
+export default catchAsync;
