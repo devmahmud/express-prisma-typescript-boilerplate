125d0dd7857fbb40830814cf7583cec41c58ec7b
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-19 17:06:26 +0600

    chore: env variable schema validations

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,26 +9,26 @@ const envVarsSchema = z.object({
   NODE_ENV: z
     .enum(['production', 'development', 'test'])
     .refine((value) => value !== undefined, { message: 'NODE_ENV is required' }),
-  PORT: z.number().default(8000),
+  PORT: z.coerce.number().default(8000),
   JWT_SECRET: z.string().default('secret').describe('JWT secret key'),
-  JWT_ACCESS_EXPIRATION_MINUTES: z
+  JWT_ACCESS_EXPIRATION_MINUTES: z.coerce
     .number()
     .default(30)
     .describe('minutes after which access tokens expire'),
-  JWT_REFRESH_EXPIRATION_DAYS: z
+  JWT_REFRESH_EXPIRATION_DAYS: z.coerce
     .number()
     .default(30)
     .describe('days after which refresh tokens expire'),
-  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: z
+  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: z.coerce
     .number()
     .default(10)
     .describe('minutes after which reset password token expires'),
-  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: z
+  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: z.coerce
     .number()
     .default(10)
     .describe('minutes after which verify email token expires'),
   SMTP_HOST: z.string().describe('server that will send the emails'),
-  SMTP_PORT: z.number().describe('port to connect to the email server'),
+  SMTP_PORT: z.coerce.number().describe('port to connect to the email server'),
   SMTP_USERNAME: z.string().describe('username for email server'),
   SMTP_PASSWORD: z.string().describe('password for email server'),
   EMAIL_FROM: z.string().describe('the from field in the emails sent by the app'),
