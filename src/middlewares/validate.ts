71c038b3ea83a7eb71f742476eceb37a29194c1b
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-19 17:06:45 +0600

    chore: validation middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -2,17 +2,28 @@ import httpStatus from 'http-status';
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 import ApiError from '@/utils/ApiError';
+import pick from '@/utils/pick';
 
 const validate =
   (schema: z.ZodObject<any, any>) => (req: Request, res: Response, next: NextFunction) => {
     const validSchema = schema.pick({ params: true, query: true, body: true });
-    const obj = validSchema.parse(req);
 
-    const validationRes = validSchema.safeParse(obj);
+    const validationRes = validSchema.safeParse(pick(req, Object.keys(validSchema.shape)));
 
     if (!validationRes.success) {
-      const errorMessage = validationRes.error.issues.map((issue) => issue.message).join(', ');
-      return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+      const validationErrors = validationRes.error.errors.map((err) => {
+        const path = err.path.join('.');
+        return { path, message: err.message };
+      });
+      return next(
+        new ApiError(
+          httpStatus.BAD_REQUEST,
+          "Invalid request's body or query params",
+          true,
+          undefined,
+          validationErrors
+        )
+      );
     }
 
     Object.assign(req, validationRes.data);
